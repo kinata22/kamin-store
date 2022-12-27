d8a8013f2629fa04ba51060d00f448884d8ebf43
nkyrylenko <119662581+nkyrylenko@users.noreply.github.com>
2022-12-27 23:19:14 +0100

    feat: product details page

--- src/ts/modules/app.ts
+++ src/ts/modules/app.ts
@@ -1,15 +1,20 @@
 import { Routes } from './routes/routes';
 import ProductsList from './products-list/products-list';
+import ProductDetails from './product-details/product-details';
 
 class App {
     route: Routes;
     productsList: ProductsList;
+    productDetails: ProductDetails;
     constructor() {
         this.route = new Routes();
         this.productsList = new ProductsList(this.route);
         this.productsList.draw();
         this.productsList.drawSide();
         this.productsList.drawPages();
+
+        this.productDetails = new ProductDetails(1);
+        this.productDetails.drawDetails();
     }
 }
 
